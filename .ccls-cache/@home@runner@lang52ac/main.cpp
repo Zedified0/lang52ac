7145e1405fb8231818a6b356262e05761c91b81d
Zedified0 <143820524+Zedified0@users.noreply.github.com>
2023-09-06 14:25:01 +0000

    completed

--- .ccls-cache/@home@runner@lang52ac/main.cpp
+++ .ccls-cache/@home@runner@lang52ac/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  cout << "Enter Length and Width" << endl;
+  int l;
+  int w;
+  cin >> l;
+  cin >> w;
+  int a = l*w;
+  int p = (l+w)*2;
+  cout << "Area: " << a << endl;
+  cout << "Perimeter: " << p;
 }
